8a30ed94b932a517f26dacee6b841255c2583942
chawn <chawput@gmail.com>
2023-10-16 15:17:23 +0700

    [feat] เพิ่มเป็น 4 layer

--- src/App.tsx
+++ src/App.tsx
@@ -12,23 +12,55 @@ const center = {  lat: 13.658499097970763, lng: 100.61366111771123 }
 function MyComponent() {
 	const { isLoaded } = useJsApiLoader({
 		id: 'google-map-script',
-		googleMapsApiKey: 'YOUR_API_KEY',
+		googleMapsApiKey: 'YOUR API KEYS',
 	});
 
 	const [map, setMap] = React.useState(null);
+	const [map2, setMap2] = React.useState(null);
 
 	const onLoad = React.useCallback(function callback(map: any) {
-		// This is just an example of getting and using the map instance!!! don't just blindly copy!
-		const bounds = new window.google.maps.LatLngBounds(center);
-		// map.fitBounds(bounds);
-		map.data.loadGeoJson("http://localhost:5174/src/assets/map.json");
-		
-		map.data.setStyle({
-			fillColor: 'green',
-			strokeWeight: 1
-		});
-
 		setMap(map);
+
+    // Load and style the first GeoJSON layer
+    map.data.loadGeoJson('http://localhost:5174/src/assets/hybiot.json');
+    map.data.setStyle({
+      fillColor: 'green',
+      strokeWeight: 1,
+    });
+
+    // Create a new Data layer for the second GeoJSON
+    const secondLayer = new window.google.maps.Data();
+    secondLayer.loadGeoJson('http://localhost:5174/src/assets/io.json');
+    secondLayer.setStyle({
+      fillColor: 'red',
+      strokeWeight: 1,
+    });
+
+    // Add the second layer to the map
+    secondLayer.setMap(map);
+
+
+		 // Create a new Data layer for the second GeoJSON
+		 const thirdLayer = new window.google.maps.Data();
+		 thirdLayer.loadGeoJson('http://localhost:5174/src/assets/wow.json');
+		 thirdLayer.setStyle({
+			 fillColor: 'purple',
+			 strokeWeight: 1,
+		 });
+ 
+		 // Add the second layer to the map
+		 thirdLayer.setMap(map);
+
+		  // Create a new Data layer for the second GeoJSON
+			const fourthLayer = new window.google.maps.Data();
+			fourthLayer.loadGeoJson('http://localhost:5174/src/assets/face.json');
+			fourthLayer.setStyle({
+				fillColor: 'yellow',
+				strokeWeight: 1,
+			});
+	
+			// Add the second layer to the map
+			fourthLayer.setMap(map);
 	}, []);
 
 	const onUnmount = React.useCallback(function callback(ma: any) {
@@ -43,7 +75,6 @@ function MyComponent() {
 			onLoad={onLoad}
 			onUnmount={onUnmount}
 		>
-			{/* Child components, such as markers, info windows, etc. */}
 			<></>
 		</GoogleMap>
 	) : (
